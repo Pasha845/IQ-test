f2b6c9555491446811daad406d20acc0d6290298
Павел Горулев <pasha.gorulev@gmail.com>
2023-04-20 18:35:17 +0300

    Верстка

---
+++ js/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('1', '1.html', true),            
+            new Route('2', '2.html'),
+            new Route('3', '3.html'),
+            new Route('4', '4.html'),
+            new Route('5', '5.html'),
+            new Route('6', '6.html'),
+            new Route('7', '7.html'),
+            new Route('8', '8.html'),
+            new Route('9', '9.html'),
+            new Route('10', '10.html'),
+            new Route('11', '11.html'),
+            new Route('12', '12.html')
+        ]);
+    }
+    init();
+}());
